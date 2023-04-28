60fde0cf6d86d626ba71282acde9d64767f236a1
junstdoitjun <bing74700@gmail.com>
2023-04-28 09:51:55 +0900

    0427 - lecture

---
+++ src/test/java/com/kbstar/web09/Web09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbstar.web09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
